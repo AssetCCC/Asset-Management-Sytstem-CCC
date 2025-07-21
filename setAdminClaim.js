5966c04a34cf86c54b05c0b9430b81063d395e67
AssetCCC <shane_dorgan@corkcity.ie>
2025-07-21 16:09:10 +0100

    Add files via upload

---
+++ setAdminClaim.js
@@ -0,0 +1,19 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Replace with your user's UID
+const uid = 'BHmp9PT1PUhMEC8XkpZ1yG1CQoQ2';
+
+admin.auth().setCustomUserClaims(uid, { admin: true })
+  .then(() => {
+    console.log('Admin claim set for user:', uid);
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error('Error setting admin claim:', error);
+    process.exit(1);
+  });
